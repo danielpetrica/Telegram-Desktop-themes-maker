ed9e2cdc4bd1919ed1c8fe92a56285e7c8647218
Andrei-Daniel Petrica <apetrtica@gmial.com>
2017-02-03 23:54:06 +0100

    Finished the file containing the array of all colors in a theme + coment. Used the telegram officil theme ( link: github.com/telegramdesktop/tdesktop/blob/master/Telegram/Resources/colors.palette )   new file:   app/expert_start.php 	new file:   app/playground.txt   new file:   app/theme_array_model.php

---
+++ app/expert_start.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Telegram Theme Maker Complete version</title>
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/meshki/1.4.2/meshki.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/meshki/1.4.2/meshki.min.js"></script>
+    
+  </head>
+  <body>
+
+  </body>
+</html>
